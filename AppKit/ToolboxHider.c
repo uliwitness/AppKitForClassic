555db3b7861ef0cb4b51149a69695f4e29d975a5
Uli Kusterer <ulivc@zathras.de>
2021-05-06 00:27:21 +0200

    Started work on text fields (clickable, but no text entry yet), and auto resizing masks.

---
+++ AppKit/ToolboxHider.c
@@ -0,0 +1,1 @@
+/*	Some Toolbox headers declare symbols that conflict with Objective-C symbols or reserved words.	We hide them in this plain C file and expose them via functions*/#include "ToolboxHider.h"#include <Sound.h>void NSBeep( void ) {	SysBeep( 10 );}
---
+++ AppKit/ToolboxHider.h
@@ -0,0 +1,1 @@
+void NSBeep(void);
